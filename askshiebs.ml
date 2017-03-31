8d6daea21e625c8c40258ce7cca37174655a2e22
Sam Pelletier <spelletier@college.harvard.edu>
2017-03-31 13:41:09 -0400

    time inputs

--- askshiebs.ml
+++ askshiebs.ml
@@ -77,11 +77,11 @@ future assignments.
 ......................................................................*)
 
 (* First part: Implementing the crawler *)
-let minutes_spent_on_crawl () : int = failwith "not provided" ;;
+let minutes_spent_on_crawl () : int = 240 ;;
 
 (* Second part: Sets as dictionaries *)
-let minutes_spent_on_sets () : int = failwith "not provided" ;;
+let minutes_spent_on_sets () : int = 300 ;;
 
 (* Third part: Testing the performance of the implementations *)
-let minutes_spent_on_performance () : int = failwith "not provided" ;;
+let minutes_spent_on_performance () : int = 360 ;;
 
